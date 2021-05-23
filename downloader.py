fab6515fc010c8ae9aaf6980a48e678cdb4426a1
SSSZmeY <SSSZmeY@users.noreply.github.com>
2021-05-23 22:36:35 +0300

    Add nested folders support

--- downloader.py
+++ downloader.py
@@ -24,7 +24,7 @@ def filter_file(file_filter, filename):
     return regex.search(file_filter, filename, regex.IGNORECASE)
 
 
-def fetch_files(fetch_hash, offset, sk, cookie, file_filter, files_to_download):
+def fetch_files(fetch_hash, offset, sk, cookie, file_filter, files_to_download, folder):
     """
     Fetch the rest of the files in folder
     """
@@ -37,17 +37,24 @@ def fetch_files(fetch_hash, offset, sk, cookie, file_filter, files_to_download):
     file_url_raw = file_url_resp.json()
 
     resources = file_url_raw["resources"]
+    
+    is_root_dir = True
 
     for file in resources:
         if file["type"] != "dir":
             match = filter_file(file_filter, file["name"])
             if match:
                 files_to_download.append(file["path"])
+        elif is_root_dir:
+            is_root_dir = False
+        else:
+            download_folder(file["path"], sk, cookie, file_filter, folder + "/" + file["name"])
+        
 
     return resources[0]["completed"], len(resources)
 
 
-def get_files_list(url, file_filter):
+def get_files_list(url, file_filter, folder):
     """
     Get list of files in folder filtered with regex
     """
@@ -57,9 +64,9 @@ def get_files_list(url, file_filter):
 
     get_files_raw = get_files_resp.content
     get_files_html = BeautifulSoup(get_files_raw, 'html.parser')
-
-    get_files_json_raw = get_files_html.find("script", {"id": "store-prefetch"})
-    get_files_json = json.loads(get_files_json_raw.get_text())
+    
+    get_files_json_raw = get_files_html.find("script", id = "store-prefetch")
+    get_files_json = json.loads(get_files_json_raw.string)
 
     sk = get_files_json["environment"]["sk"]
 
@@ -72,18 +79,22 @@ def get_files_list(url, file_filter):
     files_to_download = []
     offset = 0
     for file_id in files:
-        file = resources[file_id]["path"]
-        match = filter_file(file_filter, file)
+        file_resource = resources[file_id]
+        file = file_resource["path"]
+        if file_resource["type"] != "dir":           
+            match = filter_file(file_filter, file)
+            if match:
+                files_to_download.append(file)
+        else:
+            download_folder(file, sk, cookie, file_filter, folder + "/" + file_resource["name"])
         offset += 1
-        if match:
-            files_to_download.append(file)
 
     completed = root["completed"]
     while not completed:
-        completed, new_offset = fetch_files(fetch_hash, offset, sk, cookie, file_filter, files_to_download)
+        completed, new_offset = fetch_files(fetch_hash, offset, sk, cookie, file_filter, files_to_download, folder)
         offset += new_offset
 
-    print("Got " + str(len(files_to_download)) + " files matching filter: " + file_filter)
+    #print("Got " + str(len(files_to_download)) + ((" files matching filter: " + file_filter) if file_filter is not None else ""))
 
     return FileList(files_to_download, sk, cookie)
 
@@ -106,7 +117,7 @@ def get_file_url(file, sk, cookie, folder):
     Get direct file url from its ID
     """
 
-    filename = file[file.find(":/") + 2:]
+    filename = file[file.rfind("/") + 1:]
     params = json.dumps({"hash": file, "sk": sk})
     headers = {"Content-Type": "text/plain", "Host": "yadi.sk", "Cookie": cookie}
     data = parse.quote(params).encode()
@@ -118,11 +129,24 @@ def get_file_url(file, sk, cookie, folder):
     download_file(file_url, os.path.join(folder, filename))
 
 
-def main(url, folder, file_filter):
-    if not path.exists(folder):
-        os.mkdir(folder)
+def get_folder_files_list(fetch_hash, sk, cookie, file_filter, folder):
+    files_to_download = []
+    offset = 0
+    completed = False
+    while not completed:
+        completed, new_offset = fetch_files(fetch_hash, offset, sk, cookie, file_filter, files_to_download, folder)
+        offset += new_offset
+        
+    return FileList(files_to_download, sk, cookie)
 
-    file_list = get_files_list(url, file_filter)
+    
+def download_folder(fetch_hash, sk, cookie, file_filter, folder):
+    if not path.exists(folder):
+        os.mkdir(folder)    
+
+    print("Downloading " + folder)
+
+    file_list = get_folder_files_list(fetch_hash, sk, cookie, file_filter, folder)
 
     counter = 0
     count = len(file_list.files)
@@ -131,8 +155,26 @@ def main(url, folder, file_filter):
         counter += 1
         print(str(counter) + "/" + str(count))
 
-    print("Done.")
+    print("Done downloading" + folder)
+    
+
+def main(url, folder, file_filter):
+    if not path.exists(folder):
+        os.mkdir(folder)    
+
+    print("Downloading " + folder)
+
+    file_list = get_files_list(url, file_filter, folder)
+
+    counter = 0
+    count = len(file_list.files)
+    for file in file_list.files:
+        get_file_url(file, file_list.sk, file_list.cookie, folder)
+        counter += 1
+        print(str(counter) + "/" + str(count))
+
+    print("Done downloading" + folder)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2], sys.argv[3] if len(sys.argv) == 4 else None)
